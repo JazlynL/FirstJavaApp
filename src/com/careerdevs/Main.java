b2901568d05dd5c3fb5cf87246f8b0b98c41fac7
“JazlynL” <lubenjazlyn35@gmail.com>
2021-12-21 21:47:39 -0500

    completed getFirstValue algorithm

--- src/com/careerdevs/Main.java
+++ src/com/careerdevs/Main.java
@@ -5,5 +5,13 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         System.out.println("Hello GITHUB");
+        System.out.println(getFirstValue(new int[]{2, 4, 6, 8, 10}));
+
     }
-}
+
+
+        public static int getFirstValue(int[] arr) {
+            return arr[0];
+        }
+
+    }
