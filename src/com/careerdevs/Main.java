42eea9e03f72a459f58ba3d57859f7419fa6fd35
“JazlynL” <lubenjazlyn35@gmail.com>
2021-12-21 22:12:23 -0500

    completed my first addition problem on java

--- src/com/careerdevs/Main.java
+++ src/com/careerdevs/Main.java
@@ -1,11 +1,14 @@
 package com.careerdevs;
 
+import java.util.Enumeration;
+
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
         System.out.println("Hello GITHUB");
         System.out.println(getFirstValue(new int[]{2, 4, 6, 8, 10}));
+        System.out.println(addition(2 ));
 
     }
 
@@ -14,4 +17,9 @@ public class Main {
             return arr[0];
         }
 
+       public static int addition(int num) {
+        int nextnumber = num+1;
+        return nextnumber;
+
+    }
     }
