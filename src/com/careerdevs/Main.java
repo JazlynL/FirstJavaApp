05e45ae2c356e18acc206600f9239588e77a85e3
“JazlynL” <lubenjazlyn35@gmail.com>
2021-12-22 14:17:34 -0500

    completing the search of item in java.

--- src/com/careerdevs/Main.java
+++ src/com/careerdevs/Main.java
@@ -9,6 +9,7 @@ public class Main {
         System.out.println("Hello GITHUB");
         System.out.println(getFirstValue(new int[]{2, 4, 6, 8, 10}));
         System.out.println(addition(2 ));
+        System.out.println(search(new int[]{2, 4, 6, 8, 10}, 8));
 
     }
 
@@ -18,8 +19,20 @@ public class Main {
         }
 
        public static int addition(int num) {
-        int nextnumber = num+1;
-        return nextnumber;
+        int nextNum= num+1;
+        return nextNum;
 
     }
+       public static int search(int arr[], int item) {
+        for(int i=0;i<arr.length;i++)
+        {
+            if(arr[i]==item)
+                return i;
+
+        }
+        return -1;
     }
+
+
+     }
+
