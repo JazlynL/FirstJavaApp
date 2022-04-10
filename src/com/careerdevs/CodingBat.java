4662d6e5c8ea30e763a5e22a5b9f2ce73556fa43
“JazlynL” <lubenjazlyn35@gmail.com>
2022-04-10 14:48:15 -0400

    algorithim practice

---
+++ src/com/careerdevs/CodingBat.java
@@ -0,0 +1,25 @@
+package com.careerdevs;
+
+public class CodingBat {
+    public int caughtSpeeding(int speed, boolean isBirthday) {
+        // 0 = no ticket, 1 = small ticket . 2 = big ticket;
+        //if speed if 60 < result = 0 ; if speed = 61- 80 = 1  if speed is 81 + = 3
+        //if birthday 65 = 0 ; 66 - 65 = 1; 83 = 3
+        if(isBirthday == false){
+            if(speed <= 60)
+                return 0;
+            else if (speed >= 61 && speed <= 80)
+                return 1;
+            else if( speed >= 81)
+                return 2;
+        }
+        if(speed <= 65){
+            return 0;
+        }else if ( speed >= 66 && speed <=85){
+            return 1;
+        }else {
+            return 2;
+        }
+
+    }
+}
--- src/com/careerdevs/recursivept2/ReverseString.java
+++ src/com/careerdevs/recursivept2/ReverseString.java
@@ -4,7 +4,7 @@ public class ReverseString {
     public static String reverseString( String str) {
         StringBuilder reversed = new StringBuilder();
         for (int i = str.length() -1 ; i >= 0 ; i--){
-            StringBuilder append = reversed.append(str[i]);
+            reversed.append(str[i])
         }
         return reversed.toString();
     }
