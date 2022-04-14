6284dcd6d0f1ee058dc7546f1bd11422ace57605
“JazlynL” <lubenjazlyn35@gmail.com>
2022-04-13 23:41:28 -0400

    doing class algorithim problems.

--- src/com/careerdevs/recursivept2/ReverseString.java
+++ src/com/careerdevs/recursivept2/ReverseString.java
@@ -4,6 +4,7 @@ public class ReverseString {
     public static String reverseString( String str) {
         StringBuilder reversed = new StringBuilder();
         for (int i = str.length() -1 ; i >= 0 ; i--){
+            // acceessing the array and appending it.
             reversed.append(str[i])
         }
         return reversed.toString();
