f56d458afde9e99760adde8a5df133dfaa80a298
“JazlynL” <lubenjazlyn35@gmail.com>
2022-05-04 17:11:26 -0400

    completeting edabitScoringChallange

---
+++ src/com/careerdevs/edabitScoringChallange.java
@@ -0,0 +1,24 @@
+package com.careerdevs;
+
+public class edabitScoringChallange {
+    public static int[] calculateScores(String str) {
+        int player1 = 0;
+        int player2 = 0;
+        int player3 = 0;
+
+        for(int i = 0 ; i < str.length();i++){
+            if(str.charAt(i) == 'A'){
+                player1++;
+            }if(str.charAt(i) == 'B')
+                player2++;
+
+            if(str.charAt(i)== 'C'){
+                player3++;
+            }
+        }
+        return new int [] {player1,player2,player3};
+
+
+    }
+}
+}
