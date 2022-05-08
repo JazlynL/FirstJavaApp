82e9c48878b09ff55c30e4d7def7f3b0ae1dabb1
“JazlynL” <lubenjazlyn35@gmail.com>
2022-05-07 22:13:33 -0400

    completed getFactor, GetXO algorithim.

---
+++ src/com/careerdevs/getXO.java
@@ -0,0 +1,22 @@
+package com.careerdevs;
+
+public class getXO {
+    public static boolean getXO (String str) {
+        String newStr = str.toLowerCase();
+        int containX = 0;
+        int  containO = 0;
+
+        for(int i = 0 ; i < newStr.length(); i++){
+            if(newStr.charAt(i) == 'x'){
+                containX++;
+            }if(newStr.charAt(i) == 'o'){
+                containO++;
+            }
+        }
+        if(containX == containO){
+            return true;
+        }
+        return false;
+    }
+
+}
