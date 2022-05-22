90f6d546501c72c91638cac352f41339eafede1b
“JazlynL” <lubenjazlyn35@gmail.com>
2022-05-21 21:37:51 -0400

    completed reverese algorithim

--- src/com/careerdevs/getXO.java
+++ src/com/careerdevs/getXO.java
@@ -5,6 +5,7 @@ public class getXO {
         String newStr = str.toLowerCase();
         int containX = 0;
         int  containO = 0;
+        // using this to set it equal to eachother
 
         for(int i = 0 ; i < newStr.length(); i++){
             if(newStr.charAt(i) == 'x'){
