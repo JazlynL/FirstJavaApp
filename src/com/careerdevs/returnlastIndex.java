3618c5991b213cff1ea00649329b5b1fdc1f4599
“JazlynL” <lubenjazlyn35@gmail.com>
2022-05-27 21:34:09 -0400

    returned last index of.

---
+++ src/com/careerdevs/returnlastIndex.java
@@ -0,0 +1,13 @@
+package com.careerdevs;
+
+public class returnlastIndex {
+    public static int search(int arr[], int item) {
+        for(int i = 0 ; i < arr.length ; i++){
+            if(arr[i]== item){
+                return i;
+            }
+        }
+        return -1;
+    }
+
+}
