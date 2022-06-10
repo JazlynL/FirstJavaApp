c272e398383c8a694cfcdc8376b6da8e19403852
“JazlynL” <lubenjazlyn35@gmail.com>
2022-06-10 12:24:29 -0400

    practice on coding bat

---
+++ src/com/careerdevs/recursivept2/CodingBatAlgo/ContainsDoubleX.java
@@ -0,0 +1,21 @@
+package com.careerdevs.recursivept2.CodingBatAlgo;
+
+public class ContainsDoubleX {
+    boolean doubleX(String str) {
+        int counter=0;
+        for(int i = 0 ; i < str.length()-1;i++){
+            if(str.charAt(i) == 'x'){
+
+                if(str.charAt(i+1) == 'x'){
+                    counter++;
+                    return true;
+
+                }
+                return false;
+            }
+
+        }
+        return false;
+
+    }
+}
