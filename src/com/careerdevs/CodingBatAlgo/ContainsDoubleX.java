1d4b3fe344bebfb418fdd952448943d73a3e81c1
“JazlynL” <lubenjazlyn35@gmail.com>
2022-07-03 18:24:33 -0400

    another alternative solution

--- src/com/careerdevs/CodingBatAlgo/ContainsDoubleX.java
+++ src/com/careerdevs/CodingBatAlgo/ContainsDoubleX.java
@@ -18,4 +18,19 @@ public class ContainsDoubleX {
         return false;
 
     }
+    boolean doubleXTwo(String str) {
+        // var is j == str.index of x
+        int j = str.indexOf("x");
+//if j is out of bounds which is repersented -1 return false;
+        if( j == -1) return false;
+
+        // if j +1 is greater than str.length
+        if(j+1>=str.length())
+            // if  J+1 is true return false
+            return false;
+
+
+// return true if j+1 and j+2 equals x
+        return str.substring(j+1,j+2).equals("x");
+    }
 }
