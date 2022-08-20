0213898fe3dc0a26e3ce2f98e7cdc8bf69588f7e
“JazlynL” <lubenjazlyn35@gmail.com>
2022-08-20 14:44:50 -0400

    Day 7 hackerrank

---
+++ src/com/careerdevs/ThirtyDaysOfCode/Day7reverse.java
@@ -0,0 +1,32 @@
+package com.careerdevs.ThirtyDaysOfCode;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Day7reverse {
+
+
+    public class Solution {
+        public static void main(String[] args) throws IOException {
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+            int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+            String[] arrTemp = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+            List<Integer> arr = new ArrayList<>();
+
+            for (int i = 0; i < n; i++) {
+                int arrItem = Integer.parseInt(arrTemp[i]);
+                arr.add(arrItem);
+            }
+            for( int i = n-1 ; i >=0 ; i--){
+                System.out.println(arr.get(i));
+            }
+
+
+        }
+    }
