1eb163337ee18926b3eab85378cfc975e476fd31
“JazlynL” <lubenjazlyn35@gmail.com>
2022-09-16 01:16:12 -0400

    HackerRank Algorithim practice Day 25 completed, diving through Time complexity/Space Complexity in Algorithimic function run times.

---
+++ src/com/careerdevs/ThirtyDaysOfCode/Day25TimeComplexity.java
@@ -0,0 +1,34 @@
+package com.careerdevs.ThirtyDaysOfCode;
+
+public class Day25TimeComplexity {
+
+    /* Notes!!!
+  Ask developers we ask what is the runtime complexity
+  -- how fast is a given function Based on a given input of
+  Size N...
+  --How Fast the code runs in proportion to its input..
+  --
+  4 categories of Complexities.
+  first running time is
+  O (1) -> constant..
+  Doesn't depend on the input.
+
+  O(log(n)) -> logrimithic
+
+  O(N) -> linear you go through each element once.
+
+  **the size od the problem is the size of the solution
+
+  input--time
+  -----------
+   10 // 10 secs
+   100 // 100 secs
+   1000//1000 secs
+
+
+  O(n ^2 ) Quadratic (nested for loops)
+
+
+
+    * */
+}
