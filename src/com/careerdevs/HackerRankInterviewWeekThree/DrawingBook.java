feed476bea8ccf357945a04df7d8604430288055
JazlynL <lubenjazlyn35@gmail.com>
2022-11-09 14:40:18 -0800

    solved drawing book.

---
+++ src/com/careerdevs/HackerRankInterviewWeekThree/DrawingBook.java
@@ -0,0 +1,42 @@
+public static int pageCount(int n, int p) {
+        // Write your code here
+
+
+
+
+        int pagesCountMin = p/2;
+
+
+
+
+// reverse for loop on n
+
+
+        int difference = n-p;
+
+        if(difference == 1){
+        difference = n%2 == 0 ? 1 : 0;
+
+        }else{
+
+        difference = difference/2;
+        }
+
+        if(pagesCountMin < difference){
+        return pagesCountMin;
+        }else{
+        return difference;
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+        }
