494b4bdf8385f291bd7740c4c341b7e4e7dc8121
JazlynL <lubenjazlyn35@gmail.com>
2022-12-25 20:07:12 -0800

    higher than mysql.

--- src/com/careerdevs/MySQL/HigherThan75.java
+++ src/com/careerdevs/MySQL/HigherThan75.java
@@ -1,3 +1,5 @@
 
 // using order by to  order the substr.
 Select  name from students where marks > 75 order by substr(name,-3, 3), ID asc;
+// we can also use the right condition;
+        Select  name from students where marks > 75 order by right(name, 3),ID;
