8a793a62292d7e2480a7cdab1b291e5c5b67e4d7
JazlynL <lubenjazlyn35@gmail.com>
2022-12-30 10:54:40 -0800

    completed simple algorithim.

---
+++ src/com/careerdevs/CodingBatAlgo/Sleepin.java
@@ -0,0 +1,11 @@
+public boolean sleepIn(boolean weekday, boolean vacation) {
+        if(!weekday && !vacation || weekday && vacation || !weekday && vacation){
+        return true;
+        }
+
+        return false;
+
+
+
+
+        }
