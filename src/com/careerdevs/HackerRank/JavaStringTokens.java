f4e8ca3de1574363bbe4b36120409f207e3c78fc
JazlynL <lubenjazlyn35@gmail.com>
2023-01-10 19:48:07 -0800

    String hackerrank token.

---
+++ src/com/careerdevs/HackerRank/JavaStringTokens.java
@@ -0,0 +1,27 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String s = scan.nextLine();
+        // Write your code here.
+        String newStr = s.replaceAll("[^a-zA-Z0-9\\s+]", " ").trim();
+
+        // System.out.println(newStr);
+        StringTokenizer st = new StringTokenizer(newStr," ");
+
+        System.out.println(st.countTokens());
+        while(st.hasMoreTokens()){
+            System.out.println(st.nextElement());
+
+        }
+
+
+
+
+
+        scan.close();
+    }
+}
