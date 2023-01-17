7ac733312c97449544c9d9231be7fe1c1f81acdf
JazlynL <lubenjazlyn35@gmail.com>
2023-01-16 20:22:36 -0800

    created a regex solution for a hackerrank challenge on java.

---
+++ src/com/careerdevs/HackerRank/Regex.java
@@ -0,0 +1,20 @@
+
+public class Solution
+{
+    public static void main(String[] args){
+        Scanner in = new Scanner(System.in);
+
+        int testCases = Integer.parseInt(in.nextLine());
+
+        for(int i = 0 ; i < testCases;i++){
+            String pattern = in.nextLine();
+            //Write your code
+            try{
+                Pattern.compile(pattern);
+                System.out.println("Valid");
+            }catch(PatternSyntaxException e){
+                System.out.println("Invalid");
+            }
+        }
+    }
+}
