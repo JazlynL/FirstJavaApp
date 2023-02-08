6ab040f09b0b710c5e2f56fda26301fb5180492f
JazlynL <lubenjazlyn35@gmail.com>
2023-02-08 11:57:13 -0800

    algorithim practice.

---
+++ src/com/careerdevs/HackerRank/GenericType.java
@@ -0,0 +1,16 @@
+class Printer
+{
+    //Write your code here
+
+
+
+    void printArray(Object [] arr){
+        for(int i  = 0 ; i < arr.length;i++){
+            System.out.println(arr[i]);
+        }
+
+    }
+
+
+
+}
